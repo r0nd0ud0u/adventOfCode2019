f3bf73b92739d7a3efaaa67bbcf9c263e54949f1
rondoudou <marcpon@yahoo.fr>
2019-12-07 19:29:28 +0100

    star 2

--- a04/star.py
+++ a04/star.py
@@ -9,34 +9,28 @@ if __name__ == '__main__':
     #y = 767253
     password = 0
     digit = -1
-    prev_digit = 0
 
     for i in range(x, y):
-        increment = False
-        adjacent = False
-        more_than_2 = False
-        digit = -1
-        prev_digit = 0
-        for j in range(0, 6):
-            prev_digit = digit
+        adjacent = 1
+        double = False
+        prev_digit = get_digit(i,0)
+        bigger =False
+
+        for j in range(1, 7):
             digit = get_digit(i, j)
-            if j >= 1:
-                if digit > prev_digit:
-                    increment = False
-                    break
-                else:
-                     increment = True
-                if digit == prev_digit and adjacent == False:
-                    adjacent = True
-                    # increment = True
-                    # more_than_2 = False
-                # elif digit == prev_digit and adjacent == True:
-                #     increment = False
-                #     adjacent = False
-                #     more_than_2 = True
-                # else:
-                #     adjacent = False
-                #     increment = True
-        if increment  and adjacent:
+
+            if digit > prev_digit :
+                bigger = True
+
+            if digit == prev_digit:
+                adjacent += 1
+            else:
+                if adjacent == 2:
+                    double = True
+                adjacent = 1
+
+            prev_digit = digit
+
+        if double and not bigger:
             password += 1
     print(password)
