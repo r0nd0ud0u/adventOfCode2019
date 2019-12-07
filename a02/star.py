9727c12376f6403c88d9e97a3fcd8f72e0c47504
rondoudou <marcpon@yahoo.fr>
2019-12-07 22:53:23 +0100

    star 2

--- a02/star.py
+++ a02/star.py
@@ -1,21 +1,20 @@
+import numpy as np
 
-
-def machine_of_turing_ahah(opcode: int, a:int, b:int):
+def machine_of_turing_ahah(opcode: object, a: object, b: object) -> object:
     if opcode == 1:
         return a + b
     elif opcode == 2:
         return a*b
+    return 0
 
 
-def calculte_First_value(liste_func:list):
+def calculte_First_value(liste_func):
     for i in range(0, len(liste_func), 4):
-        print(i)
         if liste_func[i] == 99:
-            print(liste_func[0])
             return liste_func[0]
         else:
-            liste_func[liste_func[i + 3]] = machine_of_turing_ahah(liste_func[i], liste_func[list_nb[i + 1]],
-                                                             liste_func[liste_func[i + 2]])
+            liste_func[liste_func[i + 3]] = machine_of_turing_ahah(liste_func[i], liste_func[liste_func[i+1]],
+                                                                   liste_func[liste_func[i+2]])
 
 
 if __name__ == '__main__':
@@ -23,8 +22,21 @@ if __name__ == '__main__':
         input_file = file.read()
         list_nb_str = input_file.split(',')
         list_nb = list(map(int, list_nb_str))
-        list_nb[1] = 12
-        list_nb[2] = 2
+        list_tmp = []
+        # star 1
+        # list_nb[1] = 12
+        # list_nb[2] = 2
+        #result = calculte_First_value(list_nb)
+        #print(result)
 
-        calculte_First_value(list_nb)
-        calculte_First_value(list_nb)
+        # star 2
+        a = np.array(list_nb)
+        for k in range(1, 99):
+            for j in range(1, 99):
+                list_tmp = np.copy(a)
+                list_tmp[1] = k
+                list_tmp[2] = j
+                result = calculte_First_value(list_tmp)
+                if result == 19690720:
+                    print(k, j)
+
