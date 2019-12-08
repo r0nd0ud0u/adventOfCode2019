62a021fddce8c6dbf613553e4b77a831bb69f3bc
rondoudou <marcpon@yahoo.fr>
2019-12-08 13:57:14 +0100

    star 2

--- a05/star.py
+++ a05/star.py
@@ -1,20 +1,13 @@
-import numpy as np
-
-
 def get_digit(number, n):
     return int(number / 10 ** n) % 10
 
 
 def machine_of_turing_ahah(opcode: object, a: object, b: object) -> object:
-    print("turing")
-    print(opcode)
+
     if opcode == 1:
-        print("opcode 1")
         return a + b
     elif opcode == 2:
-        print("opcode 2 ")
         return a * b
-    print("nada")
     return 0
 
 
@@ -31,37 +24,48 @@ def calculate_output(liste_func, input_instruction):
         opcode = liste_func[i]
         param_1 = 0
         param_2 = 0
-        if opcode > 10:
-            print(opcode)
+        if opcode > 10 and not opcode == 99:
             opcode = get_digit(liste_func[i], 0)
-            print(opcode)
             param_1 = get_digit(liste_func[i], 2)
-            print(param_1)
             param_2 = get_digit(liste_func[i], 3)
-            print(param_2)
         if opcode == 3:
-            print("opcode 3 ")
             liste_func[liste_func[i+1]] = input_instruction
-            print(input_instruction)
             i += 2
         elif opcode == 4:
-            print("opcode 4")
             input_instruction = liste_func[liste_func[i + 1]]
-            print(input_instruction)
             i += 2
+        elif opcode == 99:
+            return input_instruction
         else:
+            value_1 = get_value_with_param(liste_func, liste_func[i + 1], param_1)
+            value_2 = get_value_with_param(liste_func, liste_func[i + 2], param_2)
             if opcode == 2 or opcode == 1:
-                value_1 = get_value_with_param(liste_func, liste_func[i + 1], param_1)
-                print(value_1)
-                value_2 = get_value_with_param(liste_func, liste_func[i + 2], param_2)
-                print(value_2)
-                print(opcode)
                 liste_func[liste_func[i + 3]] = machine_of_turing_ahah(opcode, value_1, value_2)
-                print(liste_func[liste_func[i + 3]])
+                i += 4
+            elif opcode == 5:
+                if not value_1 == 0:
+                    i = value_2
+                else:
+                    i += 3
+            elif opcode == 6:
+                if value_1 == 0:
+                    i = value_2
+                else:
+                    i += 3
+            elif opcode == 7:
+                if value_1 < value_2:
+                    liste_func[liste_func[i + 3]] = 1
+                else:
+                    liste_func[liste_func[i + 3]] = 0
+                i += 4
+            elif opcode == 8:
+                if value_1 == value_2:
+                    liste_func[liste_func[i + 3]] = 1
+                else:
+                    liste_func[liste_func[i + 3]] = 0
                 i += 4
 
-        if liste_func[i] == 99:
-            return input_instruction
+    return input_instruction
 
 
 if __name__ == '__main__':
@@ -70,7 +74,12 @@ if __name__ == '__main__':
         list_nb_str = input_file.split(',')
         list_nb = list(map(int, list_nb_str))
 
-        input_value = 1
+        #star 1
+        #input_value = 1
+        #output = calculate_output(list_nb, input_value)
+        #print(output)
+
+        #star 2
+        input_value = 5
         output = calculate_output(list_nb, input_value)
-        print("final value")
         print(output)
