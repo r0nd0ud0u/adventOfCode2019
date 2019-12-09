add8ce774d1ba977cb5e74b096097c5c38c86bed
rondoudou <pontdmc@skysoft.atm.com>
2019-12-09 19:01:41 +0100

    star 2

--- a08/star.py
+++ a08/star.py
@@ -11,7 +11,14 @@ if __name__ == "__main__":
         list_zero_layers = []
         list_product_1_and_2 = []
         j = 1
+        list_output = ['2'] * size_layer
         for i in input_file:
+            if list_output[j-1] == '2':
+                print(list_output[j-1])
+                list_output[j-1] = i
+                if i == '0':
+                    list_output[j-1] = ' '
+
             if i == '0':
                 nb_of_zero += 1
             if i == '1':
@@ -30,4 +37,8 @@ if __name__ == "__main__":
                 j = 1
 
         min_zero_index = list_zero_layers.index(min(list_zero_layers))
-        print(list_product_1_and_2[min_zero_index])
+        nb_c = 0
+        for k in range(1,height):
+            list_output.insert(width*k + nb_c,'\n')
+            nb_c += 1
+        print(''.join(list_output))
